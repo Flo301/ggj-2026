741549f7dd704e7dbfee2dd01ee53a996e03bc1c
rw-root <robinweller20@gmail.com>
2026-01-31 22:56:46 +0100

    - add background to InteractUI

--- scripts/InGameUi.cs
+++ scripts/InGameUi.cs
@@ -3,24 +3,26 @@ using System;
 
 public partial class InGameUi : Control
 {
-    static public InGameUi Instance { get; private set; }
+	static public InGameUi Instance { get; private set; }
 
-    private Label _interactionLabel;
+	private Label _interactionLabel;
+	private Control _interactionContainer;
 
-    public override void _Ready()
-    {
-        Instance = this;
-        _interactionLabel = GetNode<Label>("InteractLabel");
-    }
+	public override void _Ready()
+	{
+		Instance = this;
+		_interactionLabel = GetNode<Label>("%InteractLabel");
+		_interactionContainer = GetNode<Control>("%ContainerInteract");
+	}
 
-    public void ShowInteractionText(string text)
-    {
-        _interactionLabel.Text = text;
-        _interactionLabel.Visible = true;
-    }
+	public void ShowInteractionText(string text)
+	{
+		_interactionLabel.Text = text;
+		_interactionContainer.Visible = true;
+	}
 
-    public void HideInteractionText()
-    {
-        _interactionLabel.Visible = false;
-    }
+	public void HideInteractionText()
+	{
+		_interactionContainer.Visible = false;
+	}
 }
