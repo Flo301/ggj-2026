1bc0da835db8064855df63bbdf75ec77523f55a5
Flo301 <neuflo.weller@gmail.com>
2026-01-31 23:45:55 +0100

    add ConditionalNodeHidder class and implement dialog visibility logic

---
+++ scripts/ConditionalNodeHidder.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System.Linq;
+
+public partial class ConditionalNodeHidder : Node3D
+{
+    [Export]
+    public string[] DoHideWhenFlags = [];
+
+    [Export]
+    public bool AllDoesMustBeTrue = false;
+
+    [Export]
+    public string[] DontHideWhenFlags = [];
+
+    [Export]
+    public bool AllDontsMustBeTrue = false;
+
+    public override void _Ready()
+    {
+        if (ShouldHide())
+        {
+            Visible = false;
+        }
+    }
+
+    private bool ShouldHide()
+    {
+        bool doHide = false;
+        if (AllDoesMustBeTrue)
+        {
+            doHide = DoHideWhenFlags.All(x => GlobalState.Instance.HasDoneDialog(x));
+        }
+        else
+        {
+            doHide = DoHideWhenFlags.Any(x => GlobalState.Instance.HasDoneDialog(x));
+        }
+
+        if (doHide)
+        {
+            if (AllDontsMustBeTrue)
+            {
+                doHide = !DontHideWhenFlags.All(x => GlobalState.Instance.HasDoneDialog(x));
+            }
+            else
+            {
+                doHide = !DontHideWhenFlags.Any(x => GlobalState.Instance.HasDoneDialog(x));
+            }
+        }
+
+        return doHide;
+    }
+}
